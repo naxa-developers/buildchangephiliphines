726fa74b0e20948ec18b4a5cdb9c139c7229faab
nishon.tan <nishon.tan@gmail.com>
2018-05-16 18:06:40 +0545

    Created Ram Scene

--- App.js
+++ App.js
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
 import { Router, Scene } from 'react-native-router-flux';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import SecondPage from './src/SecondPage';
 import SchoolList from './src/SchoolList';
+import reducers from './src/reducers';
+import Ram from './src/Ram';
 
 class App extends Component {
 
 
   render() {
       return (
-        <Router>
-                <Scene key="root">
+        <Provider store={createStore(reducers)}>
+          <Router>
+            <Scene key="root">
+                <Scene
+                  key="SchoolList"
+                  component={SchoolList}
+                  title="School List"
+                />
 
-                        <Scene
-                          key="SchoolList"
-                          component={SchoolList}
-                          title="School List"
+                <Scene
+                  key="SecondPage"
+                  component={SecondPage}
+                  title="SecondPage"
+                  hideNavBar
+                />
 
-                        />
-
-                        <Scene
-                          key="SecondPage"
-                          component={SecondPage}
-                          title="SecondPage"
-                          initial
-                          hideNavBar
-                        />
-
-                  </Scene>
-
-        </Router>
+                <Scene
+                  key="Sita"
+                  component={Ram}
+                  title="Hari"
+                  initial
+                  hideNavBar
+                />
+                </Scene>
+          </Router>
+        </Provider>
       );
     }
   }
