3f01c7ef538bfc190e74cd27489d3f52bb140f00
nishon.tan <nishon.tan@gmail.com>
2018-05-22 15:46:49 +0545

    removed conditinal rendering

--- src/components/scenes/ComparePhotosScene.js
+++ src/components/scenes/ComparePhotosScene.js
@@ -14,53 +14,41 @@ class ComparePhotosScene extends Component {
     constructor() {
         super();
         this.state = {
-            imageViewerShown: false,
+            imageViewerShown: true,
             imageViewerCurIndex: 0
         };
     }
 
-    renderImageViewer() {
-        return (
-            <Gallery
-                style={{ flex: 1, backgroundColor: 'black' }}
-                images={[
-                { source: { uri: 'http://i.imgur.com/XP2BE7q.jpg' } },
-                { source: { uri: 'http://i.imgur.com/5nltiUd.jpg' } },
-                { source: { uri: 'http://i.imgur.com/6vOahbP.jpg' } },
-                { source: { uri: 'http://i.imgur.com/kj5VXtG.jpg' } }
-                ]}
-            />
-        );
-    }
-
-    renderImageViewer2() {
+    renderImageList() {
 
         const images = [{
-            url: 'https://avatars2.githubusercontent.com/u/7970947?v=3&s=460',
+            url: this.props.guideline.good_photo,
             props: {
                 // headers: ...
             }
         },
         {
-            url: 'https://avatars2.githubusercontent.com/u/7970947?v=3&s=460',
+            url: this.props.guideline.bad_photo,
             props: {
                 // headers: ...
             }
         }];
-        
 
-        return (
-            <Modal visible transparent >
-            <ImageViewer imageUrls={images} />
-        </Modal>
-        );
-    }
-
-    renderImageList() {
         return (
             <View 
-                 style={styles.container}
+                style={styles.container}
             >
+
+                <Modal 
+                    visible={this.state.imageViewerShown} 
+                    transparent 
+                    onRequestClose={() => {
+                    this.setState({ ...this.state, imageViewerShown: false });
+                    }}
+                >
+                    <ImageViewer imageUrls={images} />
+                </Modal>
+
                 <Tile
                     activeOpacity={0.9}
                     icon={{ name: 'check-outline', type: 'material-community', color: '#8CC63E' }}
@@ -84,7 +72,7 @@ class ComparePhotosScene extends Component {
     render() {
         
         return (
-            this.renderImageViewer2()
+            this.renderImageList()
         );
     }
 }
