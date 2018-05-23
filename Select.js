2d28e771f7e24c9268dd60d54a5a779691b435e5
pakkibokati2012 <pakkibokati2012@github.com>
2018-05-23 18:13:00 +0545

    first pulled then pushed

--- Select.js
+++ Select.js
@@ -14,18 +14,22 @@ class Select extends Component {
           <Tile
             imageSrc={require('./app_images/schools_background.jpg')}
             title="View Schools"
+            titleStyle={{ fontSize: 40 }}
             height={height * 0.5}
             featured
             caption="Click to see schools in this project"
             onPress={() => Actions.Successful_Login()}
+            captionStyle={{ fontSize: 25 }}
           />
           <Tile
             imageSrc={require('./app_images/guidelines_background.jpg')}
             title="View Guidelines"
             height={height * 0.5}
+            titleStyle={{ fontSize: 40 }}
             featured
             caption="Click to explore safe construction guidelines"
             onPress={() => Actions.GuidelineCategoryScene()}
+            captionStyle={{ fontSize: 25 }}
           />
       </View>
     );
