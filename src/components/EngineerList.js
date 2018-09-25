a1f386df02678b289dbd23ce8e654e32c6e90d5c
pakkibokati2012 <pakkibokati2012@github.com>
2018-09-25 13:39:34 +0545

    solved the app intro issue

--- src/components/EngineerList.js
+++ src/components/EngineerList.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import { View, ActivityIndicator } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import call from 'react-native-phone-call';
 import { List, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 
 class EngineerList extends Component {
+
+onCallInspectorTapped(l) {
+  const args = {
+  number: l.phone_number, // String value with the number to call
+  prompt: false // Optional boolean property. Determines if the user should be prompt prior to the call
+};
+
+call(args).catch(console.error);
+}
 
   render() {
     console.log('DocumentList_bhitra');
@@ -17,8 +27,8 @@ class EngineerList extends Component {
   this.props.engineers.map((l, i) => (
     <ListItem
       roundAvatar
-      avatar={require('../../app_images/pdf.png')}
-      onPressRightIcon={() => Actions.ShowDocuments({ path: l.file })}
+      avatar={require('../../app_images/person.png')}
+      onPressRightIcon={this.onCallInspectorTapped.bind(this, l)}
       key={i}
       title={l.user}
       containerStyle={{ paddingTop: 20, paddingBottom: 20, paddingLeft: 15, paddingRight: 15, borderColor: '#EFEFF4', borderWidth: 10, borderBottomWidth: 0 }}
