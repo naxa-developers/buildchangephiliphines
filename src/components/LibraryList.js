5b1d8e4166416f4b5dd7ed368eee26dc78c5f8a9
pakkibokati2012 <pakkibokati2012@github.com>
2018-09-26 12:33:30 +0545

    changed the order of arrays

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -5,6 +5,38 @@ import ListItem from './ListItem';
 
 class LibraryList extends Component {
     componentWillMount() {
+      console.log('Array sorring result');
+
+
+      const steps = this.props.list.site_steps;
+
+// function for dynamic sorting
+function compareValues(key, order='asc') {
+  return function(a, b) {
+    if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+      // property doesn't exist on either object
+        return 0;
+    }
+
+    const varA = (typeof a[key] === 'string') ?
+      a[key].toUpperCase() : a[key];
+    const varB = (typeof b[key] === 'string') ?
+      b[key].toUpperCase() : b[key];
+
+    let comparison = 0;
+    if (varA > varB) {
+      comparison = 1;
+    } else if (varA < varB) {
+      comparison = -1;
+    }
+    return (
+      (order == 'desc') ? (comparison * -1) : comparison
+    );
+  };
+}
+
+      console.log(steps.sort(compareValues('order', 'asc')))
+
         const ds = new ListView.DataSource({
             rowHasChanged: (r1, r2) => r1 !== r2
         });
--- src/components/SubStepsList.js
+++ src/components/SubStepsList.js
@@ -10,7 +10,31 @@ class SubStepsList extends Component {
             rowHasChanged: (r1, r2) => r1 !== r2
         });
 
-        this.dataSource = ds.cloneWithRows(this.props.sub_steps);
+        function compareValues(key, order='asc') {
+          return function(a, b) {
+            if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+              // property doesn't exist on either object
+                return 0;
+            }
+
+            const varA = (typeof a[key] === 'string') ?
+              a[key].toUpperCase() : a[key];
+            const varB = (typeof b[key] === 'string') ?
+              b[key].toUpperCase() : b[key];
+
+            let comparison = 0;
+            if (varA > varB) {
+              comparison = 1;
+            } else if (varA < varB) {
+              comparison = -1;
+            }
+            return (
+              (order == 'desc') ? (comparison * -1) : comparison
+            );
+          };
+        }
+
+        this.dataSource = ds.cloneWithRows(this.props.sub_steps.sort(compareValues('order', 'asc')));
     }
 
     render() {
