c3b11e3b479e4eefc76e10749ca9bbf771ccae21
pakkibokati2012 <pakkibokati2012@github.com>
2018-11-14 14:07:29 +0545

    made room for description in the checklistitem

--- src/components/CheckListItem.js
+++ src/components/CheckListItem.js
@@ -56,15 +56,11 @@ call(args).catch(console.error);
     console.log(this.props.data);
     console.log('**********');
     console.log(this.state.checked);
-    const name = getLocalizedText(
-      this.props.data.localtext,
-      this.props.data.text
-    );
 
     return (
       <View style={styles.container}>
       <CheckBox
-        title={name}
+        title={this.props.data.text}
         checkedIcon='check-square-o'
         uncheckedIcon='square-o'
         uncheckedColor='red'
@@ -73,9 +69,10 @@ call(args).catch(console.error);
           this.setState({ checked: !this.state.checked });
         }}
         checked={this.state.checked}
-        containerStyle={{ padding: 20, margin: 0, marginRight: 0, marginLeft: 0, backgroundColor: 'white' }}
+        containerStyle={{ padding: 15, margin: 0, marginRight: 0, marginLeft: 0, backgroundColor: 'white' }}
         textStyle={{ fontSize: 16 }}
       />
+      <View style={styles.subContainer}><Text style={styles.descriptionText}>{this.props.data.description}</Text></View>
       <View style={styles.buttonWrapper}>
       <TouchableOpacity
         onPress={() => Actions.ReportEngineer({ substep: this.props.data.substep, stepId: this.props.data.step })}
@@ -116,6 +113,15 @@ const styles = StyleSheet.create({
   },
 buttonWrapper: {
   flexDirection: 'row'
+},
+subContainer: {
+  padding: 15,
+  backgroundColor: 'rgba(255,255,255,0.4)',
+  borderBottomWidth: 1,
+  borderColor: 'rgba(0,0,0,0.08)'
+},
+descriptionText: {
+
 }
 });
 
--- src/test/page1.js
+++ src/test/page1.js
@@ -59,43 +59,19 @@ showImageViewer(id) {
 
     return (
       <View style={styles.container}>
-      <Modal
-               visible={this.state.imageViewerShown}
-               transparent
-               onRequestClose={() => {
-               this.setState({ ...this.state, imageViewerShown: false });
-               }}
-            >
-                    <ImageViewer
-                        imageUrls={images}
-                        index={this.state.id}
-                    />
-              </Modal>
-
-        <View style={styles.titleContainer}>
-          <Text style={styles.titleText}>{getLocalizedText(this.props.substep.local_title, this.props.substep.title)}</Text>
-        </View>
-
-        <ListView
-          dataSource={ds.cloneWithRows(this.props.substep.primary_photos)}
-          renderRow={(rowData) => {
-            return (<TouchableOpacity onPress={this.showImageViewer.bind(this, this.props.substep.primary_photos.indexOf(rowData))}><PrimaryPhoto primaryPhoto={rowData} /></TouchableOpacity>);
-          }
-          }
-        />
 
 
         <View style={[styles.buttonContainer, styles.buttonContainerGoodPhotoBadPhoto]}>
 
           { this.props.substep.good_photos.length !== 0 &&
-            <TouchableOpacity onPress={this.onGoodPhotoTapped.bind(this)} style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', backgroundColor: '#8dc540', paddingTop: 8, paddingBottom: 8, marginRight: 4, borderRadius: 5 }}>
+            <TouchableOpacity onPress={this.onGoodPhotoTapped.bind(this)} style={{ flexWrap: 'wrap', flex: 1, flexDirection: 'row', backgroundColor: '#8dc540', padding: 8, borderRadius: 5, marginRight: 4 }}>
               <Icon name={'check'} size={35} style={styles.iconStyle} />
-              <Text style={styles.buttonText}>{strings.view_good_photo_title}</Text>
+              <View style={{ flexDirection: 'column', paddingRight: 15, width: 100 }}><Text style={styles.buttonText}>{strings.view_good_photo_title}</Text></View>
             </TouchableOpacity>
           }
 
           { this.props.substep.bad_photos.length !== 0 &&
-            <TouchableOpacity onPress={this.onBadPhotoTapped.bind(this)} style={this.props.substep.call_inspector ? { flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', backgroundColor: 'red', paddingTop: 8, paddingBottom: 8, marginLeft: 4, borderRadius: 5 } : { flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', backgroundColor: 'red', paddingTop: 8, paddingBottom: 8, borderRadius: 5 }}>
+            <TouchableOpacity onPress={this.onBadPhotoTapped.bind(this)} style={this.props.substep.call_inspector ? { flex: 1, flexDirection: 'row', backgroundColor: 'red', padding: 8, borderRadius: 5 } : { flex: 1, flexDirection: 'row', backgroundColor: 'red', paddingTop: 8, paddingBottom: 8, borderRadius: 5, marginLeft: 4 }}>
               <Icon name={'close'} size={35} style={styles.iconStyle} />
               <Text style={styles.buttonText}>{strings.view_bad_photo_title}</Text>
             </TouchableOpacity>
@@ -176,6 +152,5 @@ const styles = StyleSheet.create({
     paddingLeft: 5,
     fontWeight: 'bold',
     color: 'white',
-    fontSize: 16
-  }
+    fontSize: 16  }
 });
